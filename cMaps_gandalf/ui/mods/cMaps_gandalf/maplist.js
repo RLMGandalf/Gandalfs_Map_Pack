0e526ed48799a44ae011c00e89580981976ac754
Benjamin Rianda <reptarforlife@gmail.com>
2015-01-25 04:40:24 -0800

    Added ProfX4, SpawnWars, SpawnWarsTeams, KingdomsOfEarth.

--- cMaps_gandalf/ui/mods/cMaps_gandalf/maplist.js
+++ cMaps_gandalf/ui/mods/cMaps_gandalf/maplist.js
@@ -3,5 +3,9 @@ cShareSystems.load_pas("Gandalf", [
 	"coui://ui/mods/cMaps_gandalf/systems/canyon_wars.pas",
 	"coui://ui/mods/cMaps_gandalf/systems/iceage.pas",
 	"coui://ui/mods/cMaps_gandalf/systems/prof_x.pas",
+	"coui://ui/mods/cMaps_gandalf/systems/Kingdoms_Of_Earth.pas",
+	"coui://ui/mods/cMaps_gandalf/systems/Prof_X4.pas",
+	"coui://ui/mods/cMaps_gandalf/systems/Spawn_Wars.pas",
+	"coui://ui/mods/cMaps_gandalf/systems/Spawn_Wars_Teams.pas",
 	"coui://ui/mods/cMaps_gandalf/systems/team_arena.pas"
 ]);
